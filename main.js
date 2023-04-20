e02e1226db8fa63d4815bb374d3a071d7ccfe0ee
Diegoeosuna <diegoemilioosuna@hotmail.com>
2023-04-19 21:29:01 -0600

    Primer clase de JavaS avanzado

---
+++ main.js
@@ -0,0 +1,13 @@
+/*let numero = 5;
+
+function suma(numero){
+        let total = numero + numero
+        console.log(total)
+    return total
+}
+
+suma(150)*/
+
+//Para crear un Json package, el comando "npm init -y" es lo más sencillo.
+
+const persona = { nombre: "Diego" };
